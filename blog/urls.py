88490a76fc20dd2dcd906facdf83636a113d4686
IliaIlyich <iliaekudashev@gmail.com>
2024-06-05 13:01:04 -0600

    dummy data added to the view

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts", views.posts, name="posts-page"), 
-    path("posts/<slug:slug>", views.post_detail, name="detail-page") # "slug" - the name of concept with dynamic segment. The slug is unique 
+    path("posts/", views.posts, name="posts-page"), 
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page") # "slug" - the name of concept with dynamic segment. The slug is unique 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,70 @@
 from django.shortcuts import render
+from datetime import date
 
-post_text = {"Ants":"Text about ants", "Payments":"Text about payments", "Hiking":"Text about hiking", "Baking":"Text "}
+#to have data provided from python and not hardcoded 
+# we are going to use temp var "posts"
+posts = [
+  {
+    "slug": "payment-gateway",
+    "image": "The-Process.jpeg",
+    "author": "Ilia",
+    "date": date(2024, 6, 5),
+    "title": "Payment Gateway",
+    "excerpt": "The text about the payment Gatewey. The text about the payment Gatewey. The text about the payment Gatewey.The text about the payment Gatewey.",
+    #to create a multi line string - use
+    "content": """
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    """
+  },
+  {
+    "slug": "ant-farm",
+    "image": "The-Ant.jpeg",
+    "author": "Ilia",
+    "date": date(2024, 6, 5),
+    "title": "Ant Farm",
+    "excerpt": "The text about the Ant Farm. The text about the Ant Farm. The text about the Ant Farm.The text about the Ant Farm.",
+    #to create a multi line string - use
+    "content": """
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    """
+  },
+  {
+    "slug": "hike-in-the-mountains",
+    "image": "The-Process.jpeg",
+    "author": "Ilia",
+    "date": date(2024, 6, 5),
+    "title": "Mountain hiking",
+    "excerpt": "The text about the hiking. The text about the payment Gatewey. The text about the payment Gatewey.The text about the payment Gatewey.",
+    #to create a multi line string - use
+    "content": """
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quasi accusamus, necessitatibus doloremque voluptas optio expedita numquam ducimus nam error ratione perspiciatis consequuntur praesentium. 
+    Assumenda perspiciatis repellat maxime nihil asperiores quas?
+    """
+  }
+]
 # Create your views here.
 
 def index(request):
@@ -11,7 +75,8 @@ def index(request):
           })
 
 def posts(request):
-  pass 
+  return render(request, "blog/all-posts.html")
 
-def post_detail(request):
-  pass
+#we are going to use the dynamic url - we have to specify "slug" as a context
+def post_detail(request, slug):
+  return render(request, "blog/post-detail.html")
