c22fd6483ac875cdd1b2c28257f6af6ddf9d6bef
IliaIlyich <iliaekudashev@gmail.com>
2024-07-23 14:00:11 -0600

    models + admin ready

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
-# Register your models here.
+# to customize admin page for Post
+class PostAdmin(admin.ModelAdmin):
+    # for slug Django will use slugify inder the hood
+    prepopulated_fields = {"slug":("title",)}
+    list_display = ("title", "author", "date_created")
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,24 @@ from django.utils.text import slugify
 
 class Tag(models.Model):
     caption = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        return self.caption
+
+class Author(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    # lets use the special Email field since it has a email validator 
+    # it is also possuble to customise regex pattern 
+    e_mail = models.EmailField(null=True)
+    
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     excerpt = models.CharField(max_length=300)
     image_name = models.CharField(max_length=50)
     # set the date when the post was first created
@@ -30,13 +45,3 @@ class Post(models.Model):
     def __str__(self) -> str:
         return f"Author: {Author.first_name}, {self.title}, {self.date_created}"
         
-class Author(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
-    # lets use the special Email field since it has a email validator 
-    # it is also possuble to customise regex pattern 
-    e_mail = models.EmailField(null=True)
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
-    
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
